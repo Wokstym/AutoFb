ec638756f0c9e0d1eee35c5ce88d983960dfa890
AlexMaz99 <ola_m10@onet.pl>
2020-04-19 13:56:14 +0200

    Improved display of posts

--- home/views.py
+++ home/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse
 from django.shortcuts import render
 from django.template import loader
 import facebook
-
+import json
 from .models import UserData
 
 
@@ -12,10 +12,29 @@ def index(request):
     token = userData.token
     graph = facebook.GraphAPI(token)
     default_info = graph.get_object(id='110540857220080', fields='posts')
+    info = graph.get_object("110540857220080/picture?redirect=0")
+
     posts = default_info['posts']['data']
+    profile_image = info['data']['url']
+
+    #print(posts)
+    # print(photo)
+
+    for post in posts:
+        #print(post['id'])
+        #print(post)
+        photo = graph.get_object(post['id'] + "?fields=object_id")
+        if photo.get('object_id'):
+            photo = graph.get_object(photo['object_id'] + "/picture")
+            photo = photo['url']
+            post['url'] = photo
+            #print(post)
+        else:
+            post['url'] = None
 
     template = loader.get_template('home/index.html')
     context = {
         'posts': posts,
+        'image_url': profile_image,
     }
     return HttpResponse(template.render(context, request))
