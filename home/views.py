49abb7e93925704ff5607c4e0e8aa0b9e5e09f69
Grzegorz Poreba <grzegorz.poreba.73@gmail.com>
2020-05-03 18:33:14 +0200

    Reverted changes to master

--- home/views.py
+++ home/views.py
@@ -15,46 +15,43 @@ def index(request):
     current_user = request.user
     userData = UserData.objects.get(user_id=current_user.id)
     print(userData)
-    # token = userData.token
-    # graph = facebook.GraphAPI(token)
-    #
-    # # page_id = userData.pages.page_id
-    # page_id = '110540857220080'
-    # default_info = graph.get_object(id=page_id, fields='posts')
-    # info = graph.get_object(page_id + "/picture?redirect=0")
-    #
-    # posts = default_info['posts']['data']
-    # profile_image = info['data']['url']
-    #
-    # #print(posts)
-    # # print(photo)
-    #
-    # for post in posts:
-    #     #print(post['id'])
-    #     #print(post)
-    #     photo = graph.get_object(post['id'] + "?fields=object_id")
-    #     if photo.get('object_id'):
-    #         photo = graph.get_object(photo['object_id'] + "/picture")
-    #         photo = photo['url']
-    #         post['url'] = photo
-    #         #print(post)
-    #     else:
-    #         post['url'] = None
-    #
-    # template = loader.get_template('home/index.html')
-    # context = {
-    #     'posts': posts,
-    #     'image_url': profile_image,
-    # }
-    # paginator = Paginator(context['posts'], 3)
-    # page = request.GET.get('page')
-    # context['posts'] = paginator.get_page(page)
-    #
-    # # return HttpResponse(template.render(context, request))
+    token = userData.token
+    graph = facebook.GraphAPI(token)
+
+    # page_id = userData.pages.page_id
+    page_id = '110540857220080'
+    default_info = graph.get_object(id=page_id, fields='posts')
+    info = graph.get_object(page_id + "/picture?redirect=0")
+
+    posts = default_info['posts']['data']
+    profile_image = info['data']['url']
+
+    #print(posts)
+    # print(photo)
+
+    for post in posts:
+        #print(post['id'])
+        #print(post)
+        photo = graph.get_object(post['id'] + "?fields=object_id")
+        if photo.get('object_id'):
+            photo = graph.get_object(photo['object_id'] + "/picture")
+            photo = photo['url']
+            post['url'] = photo
+            #print(post)
+        else:
+            post['url'] = None
+
+    template = loader.get_template('home/index.html')
     context = {
-    #     'posts': posts,
-    #     'image_url': profile_image,
-     }
+        'posts': posts,
+        'image_url': profile_image,
+    }
+    paginator = Paginator(context['posts'], 3)
+    page = request.GET.get('page')
+    context['posts'] = paginator.get_page(page)
+
+    # return HttpResponse(template.render(context, request))
+
     return render(request, 'home/index.html', context)
     # return render(request, 'home/index.html')
 
