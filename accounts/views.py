0e2e5d0af1e256a8dec9affe390941422088e243
Grzegorz Poreba <grzegorz.poreba.73@gmail.com>
2020-05-15 02:18:16 +0200

    some progress in saving and cachign to database, problems with embened fields

--- accounts/views.py
+++ accounts/views.py
@@ -3,7 +3,7 @@ from django.contrib.auth.forms import UserCreationForm
 from django.shortcuts import render, redirect
 from django.contrib.auth import login
 
-from home.models import UserData, BannedWord, Page
+from home.models import UserData, BannedWord, Page, Statistics
 from .forms import SignUpForm, InsertToken, validate_token_page_id
 
 
@@ -24,19 +24,21 @@ def signup(request):
 def get_token(request):
     if request.method == 'POST':
         form = InsertToken(request.POST)
-        token = form.cleaned_data['token']
-        page_id = form.cleaned_data['page_id']
+        print(type(form))
 
-        if form.is_valid() and validate_token_page_id(token, page_id):
-
-            current_user = request.user
-            userData = UserData.objects.get(user_id=current_user.id)
-
-            page = Page(page_id=page_id, token=token, words=[])
-            userData.pages.append(page)
-            userData.save()
-
-            return redirect('home')
+        if form.is_valid():
+            token = form.cleaned_data['token']
+            page_id = form.cleaned_data['page_id']
+            if validate_token_page_id(token, page_id):
+                current_user = request.user
+                userData = UserData.objects.get(user_id=current_user.id)
+                statistics = Statistics(top_commenters=[])
+                page = Page(page_id=page_id, token=token, words=[], statistics=statistics)
+                userData.pages.append(page)
+                userData.save()
+                return redirect('home')
+            else:
+                return render(request, 'accounts/add_page.html', {'form': form, 'isValid': False})
         else:
             return render(request, 'accounts/add_page.html', {'form': form, 'isValid': False})
     else:
